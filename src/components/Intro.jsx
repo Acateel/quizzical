3c3794595b3df101cc3da3c4857f798f20da6a85
Taras Kozodoi <acateely@gmail.com>
2022-12-26 18:09:15 +0200

    add intro component

---
+++ src/components/Intro.jsx
@@ -0,0 +1,11 @@
+import './Intro.css'
+
+export default function Intro() {
+    return (
+        <div className="intro">
+            <h1>Quizzical</h1>
+            <p>Some desctiprion if needed</p>
+            <button>Start quiz</button>
+        </div>
+    )
+}
